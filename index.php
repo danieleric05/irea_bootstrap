da992efbfd8a0d329b3c674f6ab0aa092452f6c0
Aboussou Akré Daniel-Eric <aboussoudaniel@gmail.com>
2020-02-13 09:22:26 +0000

    3

--- index.php
+++ index.php
@@ -1,87 +1,42 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
 
     <title>IREA</title>
-  </head>
-  <body>
-    <header>
-        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-nav d-lg-none d-xl-none d-block d-md-block d-sm-block">
-                <a class="navbar-brand" href="#">
-                    <img src="img/logo-irea.jpg" width="30" height="30" alt="">
-                </a>
-            </div>
-            <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-                <ul class="navbar-nav">
-                <li class="nav-item d-none d-sm-none d-md-none d-lg-block d-xl-block">
-                <a class="navbar-brand py-0" href="#">
-                    <img src="img/logo-irea.jpg" width="86" height="42" alt="logo irea">
-                </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-secondary mx-2" role="button" href="#">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-secondary mx-2" href="#">Rejoignez-nous</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-secondary mx-2" role="button" href="#">Partenaires</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-secondary mx-2" role="button" href="#">Prestations</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-secondary mx-2" role="button" href="#">Contactez-nous</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-outline-secondary mx-2" role="button" href="#">Connexion</a>
-                </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+</head>
 
+<body>
     <main role="main">
-        <div class="container-fluid banner">
-            <div class="row">
-                <div class="col p-0"></div>
-                <div class="col p-0"><img src="img/bg0-1.jpg" class="img-fluid" alt=""></div>
-                <div class="col p-0"></div>
-                <div class="col p-0" style="background-color:white;"></div>
-                <div class="col p-0"></div>
+        <div class="container">
+            <div class="block marge">
+                <video class="col-md-12 col-sm-12" src="../animation/page2garde.webm" autoplay loop></video>
             </div>
-            <div class="row">
-                <div class="col p-0"></div>
-                <div class="col p-0"></div>
-                <div class="col p-0"><img src="img/bg0-3.jpg" class="img-fluid" alt=""></div>
-                <div class="col p-0"></div>
-                <div class="col p-0"><img src="img/bg0-4.jpg" class="img-fluid" alt=""></div>
-            </div>
-            <div class="row">
-                <div class="col p-0"></div>
-                <div class="col p-0"><img src="img/bg0-2.jpg" class="img-fluid" alt=""></div>
-                <div class="col-5 p-0">Cabinet d’Expertise International spécialisé dans l’immobilier et le foncier.
-EXPERTISE IMMOBILIÈRE, FONCIÈRE & JUDICIAIRE STRATÉGIE IMMOBILIÈRE & FONCIÈRE INGÉNIERIE FONCIÈRE INGÉNIERIE EN INFRASTRUCTURE & VRD ASSISTANCE & CONSEILS</div>
-                <div class="col p-0"><img src="" class="img-fluid" alt=""></div>
+            <div class="block">
+                <a href="index1.php" class="btn btn-primary">Accéder au site</a>
             </div>
         </div>
     </main>
 
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+    <!-- jQuery first, then Popper.js, then kBootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
